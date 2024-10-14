ffdf3701c8fd5cef0677a943c989ecf726f198d5
Joseph-Meiste <jomeeb2@student.oaisd.org>
2024-10-14 14:13:20 +0000

    addition

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Provide 1 number to perform the addition operation.");
+        int x = int.Parse(Console.ReadLine());
+
+        Console.WriteLine(x + " …got it. Provide a number to add to " + x + ".");
+        int y = int.Parse(Console.ReadLine());
+
+        int result = (x + y);
+
+        Console.WriteLine(y + ", alright! That’s easy. " + x + " + " + y + " is " + result + "!");
+    }
+}
