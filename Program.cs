ba6ce1572d25a871a19b9e1a36b5db6e1f8469dd
Joseph-Meiste <jomeeb2@student.oaisd.org>
2024-10-14 14:41:32 +0000

    finished product

--- Program.cs
+++ Program.cs
@@ -5,14 +5,68 @@ class Program
 
     public static void Main(string[] args)
     {
-        Console.WriteLine("Provide 1 number to perform the addition operation.");
-        int x = int.Parse(Console.ReadLine());
+        Console.WriteLine("What operation are you using? 1.Addition 2.Subtraction 3.Multiplication 4.Division 5.Modulus");
+        int op = int.Parse(Console.ReadLine());
 
-        Console.WriteLine(x + " …got it. Provide a number to add to " + x + ".");
-        int y = int.Parse(Console.ReadLine());
+        if (op == 1)
+        {
+            Console.WriteLine("Provide 1 number to perform the addition operation.");
+            int x = int.Parse(Console.ReadLine());
 
-        int result = (x + y);
+            Console.WriteLine(x + " …got it. Provide a number to add to " + x + ".");
+            int y = int.Parse(Console.ReadLine());
 
-        Console.WriteLine(y + ", alright! That’s easy. " + x + " + " + y + " is " + result + "!");
+            int add = (x + y);
+            Console.WriteLine(y + ", alright! That’s easy. " + x + " + " + y + " is " + add + "!");
+        }
+
+        else if (op == 2)
+        {
+            Console.WriteLine("Provide 1 number to perform the subtraction operation.");
+            int x = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(x + " …got it. Provide a number to subtract to " + x + ".");
+            int y = int.Parse(Console.ReadLine());
+
+            int sub = (x - y);
+            Console.WriteLine(y + ", alright! That’s easy. " + x + " - " + y + " is " + sub + "!");
+        }
+
+        else if (op == 3)
+        {
+            Console.WriteLine("Provide 1 number to perform the multiplication operation.");
+            int x = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(x + " …got it. Provide a number to multiply to " + x + ".");
+            int y = int.Parse(Console.ReadLine());
+
+            int mul = (x * y);
+            Console.WriteLine(y + ", alright! That’s easy. " + x + " * " + y + " is " + mul + "!");
+        }
+
+        else if (op == 4)
+        {
+            Console.WriteLine("Provide 1 number to perform the division operation.");
+            int x = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(x + " …got it. Provide a number to divide to " + x + ".");
+            int y = int.Parse(Console.ReadLine());
+
+            int div = (x / y);
+            Console.WriteLine(y + ", alright! That’s easy. " + x + " / " + y + " is " + div + "!");
+        }
+
+        else if (op == 5)
+        {
+            Console.WriteLine("Provide 1 number to perform the modulus operation.");
+            int x = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(x + " …got it. Provide a number to % to " + x + ".");
+            int y = int.Parse(Console.ReadLine());
+
+            int mod = (x % y);
+            Console.WriteLine(y + ", alright! That’s easy. " + x + " % " + y + " is " + mod + "!");
+        }
     }
+
 }
